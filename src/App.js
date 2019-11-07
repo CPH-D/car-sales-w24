b65810aa033f1cb6104fd2b04a1dab6e5b2b3c2a
Christopher Hernandez <cerberean661@gmail.com>
2019-11-06 19:31:59 -0800

    fixed errors, add/remove works

--- src/App.js
+++ src/App.js
@@ -6,37 +6,24 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 import { connect } from 'react-redux';
-import { removeItem, buyItem } from './actions/Actions';
-
-// const removeFeature = item => {
-//   // dispatch an action here to remove an item
-//   return {
-//     type: "REMOVE_ITEM",
-//     payload: item
-//   };
-// };
-
-// const buyItem = item => {
-//   // dispatch an action here to add an item
-//   return {
-//     type: "BUY_ITEM",
-//     payload: item
-//   };  
-// };
+import { removeItem, buyItem } from './actions/index';
 
 const App = (props) => {
   console.log(props);
   const { state, removeItem, buyItem } = props;
-
+  
   return (
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <AddedFeatures car={state.car}
+                       removeItem={removeItem}/>
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={state.store}
+                            buyItem={buyItem}/>
+        <Total car={state.car}
+               additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
@@ -48,5 +35,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(App);
-
+export default connect(mapStateToProps, { removeItem, buyItem })(App);
